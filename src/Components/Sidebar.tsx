29600dda8cb758c7c8fca1d273695324ba1c8626
Rafael Galhós <44909799+rgalhos@users.noreply.github.com>
2026-05-18 09:13:24 -0300

    web: update credits and page title

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -118,7 +118,7 @@ const SidebarContent = ({ onClose, changePage, ...rest }: SidebarProps) => {
     >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
         <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          WIMS
+          RV-SIM
         </Text>
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
--- src/Components/pages/Credits.tsx
+++ src/Components/pages/Credits.tsx
@@ -1,8 +1,28 @@
+import { Link, Text } from '@chakra-ui/react';
 
-export default function CreditsPage(){
-    return(
+export default function CreditsPage() {
+  return (
     <>
-        
+      <h1 style={{ fontSize: 30, paddingBottom: 10 }}>Credits</h1>
+
+      <Text>
+        <Link color="blue.500" href="https://github.com/rgalhos/riscv-sim">
+          RV-SIM
+        </Link>{' '}
+        is a web-based educational RISC-V simulator created by{' '}
+        <Link color="blue.500" href="https://github.com/rgalhos">
+          @rgalhos
+        </Link>
+        . It is a fork of the{' '}
+        <Link color="blue.500" href="https://github.com/ReinaldoAssis/mips-sim">
+          WIMS
+        </Link>{' '}
+        web-based MIPS simulator created by{' '}
+        <Link color="blue.500" href="https://github.com/ReinaldoAssis">
+          @ReinaldoAssis
+        </Link>
+        .
+      </Text>
     </>
-    );
+  );
 }
